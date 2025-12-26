11d1283f5b66ea9d7958d1fd730f361d596a8e72
rasovukicevic98 <rasovukicevic98@gmail.com>
2025-12-26 12:40:33 +0100

    Created the menu.

---
+++ src/i18n/translations.ts
@@ -0,0 +1,33 @@
+export const translations = {
+  me: {
+    home: {
+      title: "Restoran Dobre Vode",
+      welcome: "Dobro došli u naš restoran.",
+      p1: "Naša priča počinje davne 2000. godine, na obali rijeke Tare. Tada smo imali samo četiri stola, veliku želju i još veću ljubav prema domaćoj hrani i gostoprimstvu.",
+      p2: "Korak po korak, zahvaljujući vjernim gostima koji su nam se iznova vraćali, rasli smo zajedno sa njima. Danas, naš restoran može da ugosti preko 100 osoba, ali je duh sa samog početka ostao isti – toplina, iskren osmijeh i osjećaj da ste došli na pravo mjesto.",
+      thanks: "Hvala vam što ste dio naše priče.",
+    },
+    contact: {
+      title: "Kontakt",
+      phone: "Telefon",
+      email: "Email",
+      location: "Lokacija",
+    },
+  },
+
+  en: {
+    home: {
+      title: "Restaurant Dobre Vode",
+      welcome: "Welcome to our restaurant.",
+      p1: "Our story began back in 2000, on the banks of the Tara River. At that time, we had only four tables, a big dream, and an even bigger love for traditional food and hospitality.",
+      p2: "Step by step, thanks to loyal guests who kept coming back, we grew together. Today, our restaurant can welcome over 100 guests, yet the spirit from the very beginning remains the same – warmth, sincere smiles, and the feeling that you’ve come to the right place.",
+      thanks: "Thank you for being part of our story.",
+    },
+    contact: {
+      title: "Contact",
+      phone: "Phone",
+      email: "Email",
+      location: "Location",
+    },
+  },
+};
